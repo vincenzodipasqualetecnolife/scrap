6aa862f0b19550f2da81608f874532c8af0af2d8
federicogiulianitecnolife <f.giuliani@tecnolife.com>
2025-03-24 11:37:01 +0100

    1.0

--- main.py
+++ main.py
@@ -1,180 +1,114 @@
-# import selenium
-# from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.chrome.options import Options as ChromeOptions
-# from webdriver_manager.chrome import ChromeDriverManager
-# from selenium.webdriver.chrome.service import Service
-# import time
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.webdriver.support.ui import WebDriverWait
-# class Mef():
-#     def __init__(self):
-#         pass
-
-#     def set_up_chrome_options(self):
-#         options = ChromeOptions()
-#         options.add_argument('--no-sandbox')  # Disable sandbox to avoid issues with ChromeDriver
-#         options.add_argument('--disable-gpu')  # Disable GPU to avoid issues with ChromeDriver
-#         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-#         return driver
-    
-#     def search_url_mef(self,driver):
-#         driver.get("https://ec.europa.eu/eurostat/web/main/data/database")
-#         driver.implicitly_wait(5)  
-
-#         try:
-#             time.sleep(5)
-            
-#             shadow_host = driver.find_element(By.TAG_NAME, "navigation-full-tree")
-            
-#             shadow_root = shadow_host.shadow_root
-#             divisione = shadow_root.find_element(By.CLASS_NAME, "tree")
-
-#             divi = divisione.find_element(By.CSS_SELECTOR, "div.children.open")
-#             navigate = divisione.find_elements(By.TAG_NAME, 'navigation-category')
-#             for i in navigate:
-#                 while navigate_new:
-#                     buttons = i.find_elements(By.CSS_SELECTOR, 'a[role="button"][aria-expanded="false"]')
-#                     for b in buttons:
-#                         b.click()
-#                         navigate_new = b.find_elements(By.TAG_NAME, 'navigation-category')
-#                         for j in navigate_new:
-#                             buttons_new = i.find_elements(By.CSS_SELECTOR, 'a[role="button"][aria-expanded="false"]')
-#                             for b in buttons:
-#                                 b.click()
-
-#             if not divi:
-#                 divi = navigate.find_elements(By.CSS_SELECTOR, "div.children")
-#                 print("divi",divi)
-            
-#             buttons = i.find_elements(By.CSS_SELECTOR, 'a[role="button"][aria-expanded="false"]')
-
-
-#                 # Esegui il clic su ciascun bottone in sequenza
-#                 for button in buttons:
-#                     try:
-#                         # Fai clic sul bottone
-#                         button.click()
-#                         divi = i.find_elements(By.TAG_NAME, "div")
-#                         print("divi",divi)
-#                         buttons = i.find_elements(By.CSS_SELECTOR, 'a[role="button"][aria-expanded="false"]')
-#                         # Attendi un momento per permettere l'espansione
-#                         time.sleep(2)  # Puoi regolare il tempo di attesa in base alla tua pagina
-#                     except Exception as e:
-#                         print(f"Errore durante il clic sul bot:",e)
-
-#             title = div_elements.find_element(By.CSS_SELECTOR, "span.tree-title.full-tree-title1").text
-#             print(div_elements)
-#             print("TITOLO:", title)
-#         except Exception as e:
-#             print("Errore:", e)
-
-        
-# if __name__ == "__main__":
-#     mef = Mef()
-#     driver = mef.set_up_chrome_options()
-#     mef.search_url_mef(driver)
-#     driver.quit()
-
-import selenium
+import time
+import json
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-import time
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 class EurostatScraper:
     
-    def set_up_chrome_options(self):
+    def set_up_driver(self):
+        """
+        Imposta e restituisce un'istanza di Chrome WebDriver con opzioni personalizzate.
+        """
         options = ChromeOptions()
-        options.add_argument('--no-sandbox')  # Disable sandbox to avoid issues with ChromeDriver
-        options.add_argument('--disable-gpu')  # Disable GPU to avoid issues with ChromeDriver
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-gpu')
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
         return driver
-    def search_url_mef(self, driver):
+
+    def open_eurostat_page(self, driver):
+        """
+        Apre la pagina Eurostat e accetta i cookie se il popup appare.
+        """
         driver.get("https://ec.europa.eu/eurostat/web/main/data/database")
-        driver.implicitly_wait(5)
-        try:
-    # Attendere che il popup dei cookie sia visibile
-            cookie_button = driver.find_element(By.XPATH, '//a[contains(text(), "Accept all cookies")]')
-            cookie_button.click()
-            print("Cookie accettati!")
-        except Exception as e:
-            print("Nessun popup per i cookie trovato:", e)
-        # Attendere fino a quando il primo div è visibile
-        try:
-            shadow_host = driver.find_element(By.TAG_NAME, "navigation-full-tree")
-            
-            shadow_root = shadow_host.shadow_root
-            divisione = shadow_root.find_element(By.CLASS_NAME, "tree")
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//a[contains(text(), "Accept all cookies")]'))
+        ).click()
+        print("Cookies accettati!")
 
-            div_principale = divisione.find_element(By.CSS_SELECTOR, "div.children.open")
-            print("Trovato il div principale",div_principale)
-        except Exception as e:
-            print(f"Errore nel trovare il div iniziale: {e}")
-            return
+    def get_category_buttons(self, navigation):
+        """
+        Estrae i pulsanti delle categorie da un elemento di navigazione.
+        """
+        return navigation.find_elements(By.CSS_SELECTOR, 'a[role="button"][aria-expanded="false"]')
 
-        navigation_categories = divisione.find_elements(By.TAG_NAME, 'navigation-category')
+    def expand_category(self, driver, button):
+        """
+        Espande una categoria facendo clic sul pulsante corrispondente.
+        """
+    
+        stack = [button]
 
-        for category in navigation_categories:
-            self.expand_all_categories(driver, category)
-
-    def expand_all_categories(self, driver, root_element):
-        queue = [root_element]  # Inizializza la coda con l'elemento principale
-
-        while queue:
-            current_element = queue.pop(0)  # Prendi il primo elemento dalla coda
-            
-            # Trova il testo all'interno di 'span'
+        while stack:
+            # Prendi la cartella in cima allo stack (l'ultimo aggiunto)
+            current_button = stack.pop()    
             try:
-                span = current_element.find_element(By.TAG_NAME, 'span').text
-                print("Elemento 0:", span)
-            except Exception as e:
-                print(f"Errore nell'estrarre il testo dal 'span': {e}")
-                continue
-            
-            # if current_element == 
-            # Trova i bottoni che non sono ancora espansi
-            buttons = current_element.find_elements(By.CSS_SELECTOR, 'a[role="button"][aria-expanded="false"]')
+
+                try:
+                    span = current_button.find_element(By.TAG_NAME, 'span').text
+                    driver.execute_script("arguments[0].click();", button)
+                    print(f"Categoria espansa: {span}")
+                except Exception as e:
+                    print(f"Errore nel cliccare sulla categoria {span}: {e}")
+
+
+                shadow_host = driver.find_element(By.TAG_NAME, "navigation-full-tree")
+                shadow_root = shadow_host.shadow_root
+                tree = shadow_root.find_element(By.CLASS_NAME, "tree")
+                children_open = tree.find_element(By.CSS_SELECTOR, "div.children.open")
+                navigations = children_open.find_elements(By.XPATH, "./*")
+
+
+                # Elenco degli elementi nella cartella corrente
+                for navigation in navigations:
+                    if navigation:  # Se è una cartella, aggiungila allo stack
+                        buttons = navigation.find_elements(By.CSS_SELECTOR, 'a[role="button"][aria-expanded="false"]')
+                        for button in buttons:
+                            stack.append(button)  # Aggiungi la cartella allo stack per esplorarla dopo
+                    else:  # Se è un file, lo stampiamo (o lo gestiamo come necessario)
+                        print(f" - Navigation category non trovata")
+            except PermissionError:
+                # Gestire eventuali permessi negati per accedere a certe cartelle
+                print(f"Permesso negato per accedere a: {current_button}")
+
+    def handle_navigation(self, driver, tree):
+        """
+        Gestisce la navigazione attraverso la struttura ad albero delle categorie.
+        """
+        children_open = tree.find_element(By.CSS_SELECTOR, "div.children.open")
+        navigations = children_open.find_elements(By.XPATH, "./*")
+        
+        for navigation in navigations:
+            buttons = self.get_category_buttons(navigation)
             
             for button in buttons:
-                try:
-                    spans = button.find_element(By.TAG_NAME, 'span').text
-                    print(f"Testo nel bottone: {spans}")
-                    
-                    # Fai clic sul bottone
-                    driver.execute_script("arguments[0].scrollIntoView();", button)  # Scrolla fino al bottone
-                    driver.execute_script("arguments[0].click();", button)  # Clicca sul bottone
+                self.expand_category(driver, button)
 
-                    time.sleep(2)
+    def run(self):
+        """
+        Funzione principale che esegue il processo di scraping.
+        """
+        # Imposta il driver e apri la pagina Eurostat
+        driver = self.set_up_driver()
+        self.open_eurostat_page(driver)
 
-                    # Trova i nuovi bottoni e le nuove categorie che sono visibili dopo il clic
-                    new_buttons = current_element.find_elements(By.CSS_SELECTOR, 'a[role="button"][aria-expanded="false"]')
-                    for new_button in new_buttons:
-                        # Aggiungi i nuovi bottoni alla coda per essere cliccati
-                        queue.append(new_button)
-                        new_span = new_button.find_element(By.TAG_NAME, 'span').text
-                        print(f"Nuovo bottone trovato: {new_span}")
+        try:
+            # Accedi alla struttura ad albero nella pagina
+            shadow_host = driver.find_element(By.TAG_NAME, "navigation-full-tree")
+            shadow_root = shadow_host.shadow_root
+            tree = shadow_root.find_element(By.CLASS_NAME, "tree")
 
-                    # # Trova nuove categorie di navigazione
-                    # new_categories = driver.find_elements(By.TAG_NAME, 'navigation-category')  # Cerca globalmente
-                    # for new_category in new_categories:
-                    #     queue.append(new_category)
-                    
-                    # print(f"Espanso {len(new_categories)} nuove categorie")
+            # Gestisci la navigazione e l'espansione delle categorie
+            self.handle_navigation(driver, tree)
 
-                except Exception as e:
-                    print(f"Errore durante il clic sul bottone: {e}")
-            
+        finally:
+            # Chiudi il driver dopo l'esecuzione
+            driver.quit()
 
-            
 if __name__ == "__main__":
     mef = EurostatScraper()
-    driver = mef.set_up_chrome_options()
-    mef.search_url_mef(driver)
-   
+    mef.run()
